1ffc0e47687ae72614bbf84b7f808c4c6eb938f3
Vishal Yadav <vy432505@gmail.com>
2025-09-12 21:48:19 +0530

    Added hello function

--- App.tsx
+++ App.tsx
@@ -2,9 +2,14 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
+
+  const hello =()=> {
+    return `Hiiii there`;
+  }
+
   return (
     <View>
-      <Text>Hi there! created new APP :)</Text>
+      <Text>Hi there! created new APP :) {hello()}</Text>
     </View>
   )
 }
