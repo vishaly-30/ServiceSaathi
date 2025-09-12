745d6534785b987305936b28d53e00dd4134e6ca
Vishal Yadav <vy432505@gmail.com>
2025-09-12 21:49:56 +0530

    Starting the application

--- App.tsx
+++ App.tsx
@@ -2,6 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const App = () => {
+
+  const hello =()=> {
+    let data = 'Hello';
+    console.log("Starting the application", data); 
+    return data;
+  }
+
   return (
     <View>
       <Text>Hi there! created new APP :)</Text>
